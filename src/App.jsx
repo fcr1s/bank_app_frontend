92e2aef13fcbc86088720a2cc8f6a2dd5ad3a6c7
fcr1s <cristobal.floresa@usach.cl>
2024-11-03 19:46:14 -0300

    Añade componente Simulacion para mostrar la página de simulación

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Register from "./components/Register"; 
 import NotFound from "./components/NotFound"; // Asegúrate de importar NotFound
 import Options from "./components/Options"; // Asegúrate de importar Options
+import Simulacion from "./components/Simulacion"; // Asegúrate de importar Simulacion
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
                   <Route path="/" element={<Home />} />
                   <Route path="/register" element={<Register />} /> 
                   <Route path="/options" element={<Options />} />
+                  <Route path="/simulacion" element={<Simulacion />} />
                   <Route path="*" element={<NotFound />} />
               </Routes>
           </div>
